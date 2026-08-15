1012214eb8e7126740c39001439ea5645eaba003
Cogito06 <2146615419@qq.com>
2026-08-16 01:01:30 +0800

    hw0a commit

---
+++ hw/hw0a/test/src/Main.java
@@ -0,0 +1,16 @@
+//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+void main() {
+    /*
+    Demonstration of while/for loop in java
+     */
+    int k = 0;
+    while(k < 10){
+        System.out.println(k);
+        k ++;
+    }
+
+    for (int i = 0; i < 10; i ++){
+        System.out.println(i);
+    }
+}
