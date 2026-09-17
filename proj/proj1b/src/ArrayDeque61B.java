951da6e293fa059fc869db0fa02602caa0a5b301
Cogito06 <2146615419@qq.com>
2026-09-17 15:08:59 +0800

    Basic Resize add get

---
+++ proj/proj1b/src/ArrayDeque61B.java
@@ -0,0 +1,135 @@
+import net.sf.saxon.expr.ItemMapper;
+
+import java.util.List;
+import java.util.function.ObjDoubleConsumer;
+import java.lang.Math;
+
+public class ArrayDeque61B<T> implements Deque61B<T>{
+
+    private T[] Items;
+    private int size;
+    private int head;
+    private int tail;
+    private int arrSize;
+
+    private static int MULTIPLE = 2;
+
+    public ArrayDeque61B(){
+        size = 0;
+        Items = (T[]) new Object[8];
+        arrSize = 8;
+        head = 0;
+        tail = 0;
+    }
+
+    private void resize(){
+        int originalSize = arrSize;
+        int newSize = arrSize * MULTIPLE;
+
+        T[] newArr;
+        newArr = (T[]) new Object[newSize];
+
+        int newHead = head + (newSize - originalSize);
+
+        // copy array
+        arrayCopy(Items, newArr, 0, tail, 0, tail);
+        arrayCopy(Items, newArr, head, originalSize-1, newHead, newSize-1);
+
+        Items = newArr;
+        head = newHead;
+        arrSize = newSize;
+    }
+
+    private void arrayCopy(T[] List1, T[] List2, int start1, int end1, int start2, int end2) {
+        if(start1 >= 0 && start2 >= 0 && end1 < List1.length && end2 < List2.length) {
+            int copySize = end1 - start1;
+
+            if (copySize != (end2 - start2)) {
+                throw new IllegalArgumentException("Length are not equal!");
+            }
+
+            for (int i = start1; i <= end1; i++) {
+                int newi = start2 + (i - start1);
+                List2[newi] = List1[i];
+            }
+        }
+        else {
+            throw new ArrayIndexOutOfBoundsException("One of your array index is out of Bound");
+        }
+    }
+
+    @Override
+    public void addFirst(T x) {
+        if(size == arrSize){
+            resize();
+        }
+
+        if(!isEmpty()) {
+            int newHead = Math.floorMod(head - 1, arrSize);
+            Items[newHead] = x;
+            head = newHead;
+        }
+        else {
+            Items[0] = x;
+        }
+
+        size++;
+    }
+
+    @Override
+    public void addLast(T x) {
+        if(size == arrSize){
+            resize();
+        }
+
+        if(!isEmpty()) {
+            int newTail = Math.floorMod(tail + 1, arrSize);
+            Items[newTail] = x;
+            tail = newTail;
+        }
+        else{
+            Items[0] = x;
+        }
+
+        size++;
+    }
+
+    @Override
+    public List<T> toList() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return (size == 0);
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        if(index >= 0 && index < size){
+            int trueIndex = Math.floorMod(index + head, arrSize);
+            return Items[trueIndex];
+        }
+        return null;
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        return null;
+    }
+}
--- proj/proj1b/tests/ArrayDeque61BTest.java
+++ proj/proj1b/tests/ArrayDeque61BTest.java
@@ -1,8 +1,10 @@
 import jh61b.utils.Reflection;
+import org.junit.Assert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.google.common.truth.Truth.assertThat;
@@ -10,14 +12,59 @@ import static com.google.common.truth.Truth.assertWithMessage;
 
 public class ArrayDeque61BTest {
 
+     @Test
+     @DisplayName("ArrayDeque61B has no fields besides backing array and primitives")
+     void noNonTrivialFields() {
+         List<Field> badFields = Reflection.getFields(ArrayDeque61B.class)
+                 .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
+                 .toList();
+
+         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
+     }
+
 //     @Test
-//     @DisplayName("ArrayDeque61B has no fields besides backing array and primitives")
-//     void noNonTrivialFields() {
-//         List<Field> badFields = Reflection.getFields(ArrayDeque61B.class)
-//                 .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
-//                 .toList();
+//     void arrCopyTest(){
+//         String[] arr1 = new String[]{"Hello", "World", "Father", "Mother", "Mr. Owl", "Mr. Crow"};
+//         String[] arr2 = new String[3];
 //
-//         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
+//         ArrayDeque61B<String> llq1 = new ArrayDeque61B<>();
+//         llq1.arrayCopy(arr1, arr2, 1, 3, 0, 2);
+//
+//         String[] Expected = new String[]{"World", "Father", "Mother"};
+//         assertThat(arr2).isEqualTo(Expected);
+//
+//         // llq1.arrayCopy(arr1, arr2, -1, 2, 0, 3);
 //     }
 
+//    @Test
+//    void resizeTest(){
+//        ArrayDeque61B<String> llq1 = new ArrayDeque61B<>();
+//        llq1.addLast("Hello");
+//        llq1.addLast("World");
+//        llq1.addLast("Father");
+//        llq1.addLast("Mother");
+//        llq1.addFirst("Mr. Owl");
+//        llq1.addFirst("Mr. Crow");
+//
+//        llq1.resize();
+//    }
+
+    @Test
+    void addFirstTest(){
+        ArrayDeque61B<String> llq1 = new ArrayDeque61B<>();
+
+        llq1.addFirst("Hello");
+        llq1.addFirst("World");
+        llq1.addFirst("Father");
+        llq1.addFirst("Mother");
+        llq1.addFirst("Mr. Owl");
+        llq1.addFirst("Mr. Crow");
+        llq1.addFirst("Seven");
+        llq1.addFirst("It seems.....");
+        llq1.addFirst("We have to resize");
+
+        List<String> expected = List.of("We have to resize", "It seems.....", "Seven", "Mr. Crow", "Mr. Owl", "Mother",
+                "Father", "World", "Hello");
+        assertThat(llq1.toList()).containsExactly(expected);
+    }
 }
